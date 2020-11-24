e5e0956be0555728ce4a512a2cf3fce46bf48a13
Nicolef Avella <48898115+NicolefAvella@users.noreply.github.com>
2020-11-23 22:01:57 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+
+def analizar_peliculas():
+
+    column_list = ['title', 'release_date', 'budget', 'revenue', 'runtime']
+    df = pd.read_csv('movies_metadata.csv', usecols=column_list)
+    result = df[(df['revenue'] > 2000000) & (df['budget'] < 1000000)]
+
+    return result
+
+
+if __name__ == '__main__':
+    peliculas_resultantes = analizar_peliculas()
+    assert isinstance(peliculas_resultantes, pd.DataFrame), "Tu función aun no devuelve un Dataframe!"
+    assert peliculas_resultantes.shape[1] == 5, "Verifica que tu Dataframe solo contenga las columnas mencionadas"
+    assert peliculas_resultantes.shape[
+               0] == 3, "Creo que debes verificar los filtros, la cantidad de películas que cumple las condiciones no es correcta"
+    print("Excelente, has aprendido a leer CSV's con python y como filtrarlos!")
